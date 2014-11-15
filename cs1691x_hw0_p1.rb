64f095f7059f80939c18169cd53b3b81f1cd76ac
Enzo <enzo.colaiezzi@gmail.com>
2014-11-15 14:13:00 +0000

    Added new comments

--- cs1691x_hw0_p1.rb
+++ cs1691x_hw0_p1.rb
@@ -1,14 +1,26 @@
-# Returns the sum of an array of integers
+# Define a method sum which takes an array of integers as an argument
+# and returns the sum of its elements.
+
+# For an empty array it should return zero.
 def sum
   # your code here
 end
 
-# Returns the sum of the two largest elements in a given array of integers
+# Define a method max_2_sum which takes an array of integers as an argument
+# and returns the sum of its two largest elements.
+
+# For an empty array it should return zero.
+
+# For an array with just one element, it should return that element.
 def max_2_sum
   # your code here
 end
 
-# Returns true if any two distinct elements in the array of integers sum to n
+# Define a method sum_to_n? which takes an array of integers and an additional 
+# integer, n, as arguments and returns true if any two distinct elements in the
+# array of integers sum to n.
+
+# An empty array or single element array should both return false.
 def sum_to_n?
   # your code here
 end
