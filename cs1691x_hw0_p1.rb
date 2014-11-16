c75419546aacf9f4f5ab2203ad01c05f74c3c224
Enzo <enzo.colaiezzi@gmail.com>
2014-11-16 21:53:49 +0000

    Final touch.

--- cs1691x_hw0_p1.rb
+++ cs1691x_hw0_p1.rb
@@ -2,8 +2,9 @@
 # and returns the sum of its elements.
 
 # For an empty array it should return zero.
-def sum
-  # your code here
+def sum int_ary
+  return 0 if int_ary.empty?
+  int_ary.reduce(:+)
 end
 
 # Define a method max_2_sum which takes an array of integers as an argument
@@ -12,8 +13,8 @@ end
 # For an empty array it should return zero.
 
 # For an array with just one element, it should return that element.
-def max_2_sum
-  # your code here
+def max_2_sum int_ary
+  sum(int_ary.sort.last(2))
 end
 
 # Define a method sum_to_n? which takes an array of integers and an additional 
@@ -21,6 +22,25 @@ end
 # array of integers sum to n.
 
 # An empty array or single element array should both return false.
-def sum_to_n?
-  # your code here
+def sum_to_n?(int_ary, n)
+  sum_of_n = int_ary.combination(2).to_a.any? { | sub_ary | sum(sub_ary) == n }
+  sum_of_n && int_ary.length > 1
 end
+
+# Testing
+
+# sum
+raise 'sum([]) != 0' unless sum([]) == 0
+raise 'sum([1,2,3,4]) != 10' unless sum([1,2,3,4]) == 10
+
+# max_2_sum
+raise 'max_2_sum([]) != 0' unless max_2_sum([]) == 0
+raise 'max_2_sum([0]) != 0' unless max_2_sum([0]) == 0
+raise 'max_2_sum([15]) != 15' unless max_2_sum([15]) == 15
+raise 'max_2_sum([1,2,3,4]) != 7' unless max_2_sum([1,2,3,4]) == 7
+raise 'max_2_sum([1,4,3,2]) != 7' unless max_2_sum([1,4,3,2]) == 7
+
+# sum_to_n?
+raise 'sum_to_n([], 3) != false' unless sum_to_n?([], 3) == false
+raise 'sum_to_n([1], 3) != false' unless sum_to_n?([1], 3) == false
+raise 'sum_to_n([1,2,3,4], 3) != true' unless sum_to_n?([1,2,3,4], 3) == true
--- cs1691x_hw0_p3.rb
+++ cs1691x_hw0_p3.rb
@@ -1,5 +1,43 @@
-# Create a book with isbn and price ($20.00)
+# Define a class BookInStock which represents a book with an isbn number, isbn,
+# and price of the book as a floating-point number, price, as attributes.
+# The constructor should accept the ISBN number (a string) as the first argument
+# and price as second argument, and should raise ArgumentError
+# (one of Ruby's built-in exception types) if the ISBN number is the empty string
+# or if the price is less than or equal to zero.
+
+# Include the proper getters and setters for these attributes.
+
+# Include a method price_as_string that returns the price of the book
+# with a leading dollar sign and trailing zeros, that is,
+# a price of 20 should display as "$20.00" and a price of 33.8
+# should display as "$33.80".
+
 # ISBN standard: http://www.isbn.it/ISBN.aspx
 class BookInStock
-  # your code here
+  def initialize(isbn, price)
+    self.isbn = isbn
+    self.price = price
+  end
+  
+  attr_reader :isbn, :price
+  
+  def isbn=(isbn)
+    if isbn.empty? then raise ArgumentError.new('Every book must have ISBN code number.') end
+    @isbn = isbn
+  end
+  
+  def price=(price)
+    if price <= 0 then raise ArgumentError.new('Every book must have price value.') end
+    @price = price
+  end
+  
+  def price_as_string
+    format "$%.2f", @price
+  end
+  
 end
+
+# Testing BookInStock
+book = BookInStock.new('9788889637159', 112.3110358)
+puts "#{book.isbn} => #{book.price}"
+puts "#{book.price_as_string}"
